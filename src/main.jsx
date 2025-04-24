907a9babe3a29ac887d36ae3e385f45cb23609e1
Aristo1989 <cristianalejo2407@gmail.com>
2025-04-24 02:11:38 -0500

    version final

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ThemeProvider } from "./components/Tema/Tema";
+import { ThemeProvider } from "./Components/Tema/Tema.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
